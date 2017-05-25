2135d0a1507151fd1e049f3b6d1d61f91167ac72
DrakeLuce <drakeluce@gmail.com>
2017-05-25 18:12:55 -0300

    Added suffix searching

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ var base = "Drake";
 var nonce = 0;
 var found = false;
 
+var suffix = "abcd";
+
 while(!found){
 	var hash = crypto.createHash('sha256').update(base + nonce).digest('base16').toString('hex');
 	console.log(nonce + ": " + hash);
-	if(hash.substring(0,4) == "0000"){
+	if(hash.substring(0,suffix.length) == suffix){
 		console.log("--------FOUND!--------");
 		console.log(base + nonce);
 		console.log(hash);

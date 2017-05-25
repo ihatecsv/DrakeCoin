45df8e4e29431b3231a37ab5f967fb0833327aee
DrakeLuce <drakeluce@gmail.com>
2017-05-25 20:58:19 -0300

    Began the construction of Merkle trees

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
 var crypto = require('crypto');
 
-//crypto.createHash('sha256').update(base + nonce).digest('base16').toString('hex');
+var sha256 = function(string){
+	return crypto.createHash('sha256').update(string).digest('base16').toString('hex');
+}
+
+class MerkleNode {
+	constructor(transaction, parentNode, childNode0, childNode1, transaction) {
+		this.parentNode = parentNode;
+		this.childNodes[0] = childNode0;
+		this.childNodes[1] = childNode1;
+		this.transaction = transaction;
+	}
+	computeHash() {
+		if(this.childNodes[0] == null){
+			this.hash = sha256(JSON.stringify(transaction));
+		}else{
+			this.hash = sha256(childNodes[0].hash + childNodes[1].hash);
+		}
+	}
+}
 
 class Transaction {
-	constructor(sender, reciever, amount, isReward) {
+	constructor(sender, reciever, amount, timestamp, isReward) {
 		this.sender = sender;
 		this.reciever = reciever;
 		this.amount = amount;
+		this.timestamp = timestamp;
 		this.isReward = isReward;
 	}
 }
 
 class Block {
-	constructor(previousHash, transactions, timestamp) {
+	constructor(previousHash, previousDepth, transactions, timestamp) {
+		this.common = 121374;
 		this.previousHash = previousHash;
+		this.previousDepth = previousDepth + 1;
 		this.transactions = transactions;
 		this.timestamp = timestamp;
 	}
 }
 
-var transactions = [
-	new Transaction("testsender1", "testreciever1", 0.005, false),
-	new Transaction("testsender2", "testreciever2", 0.002, false),
-	new Transaction("testsender3", "testreciever3", 0.008, false)
-];
+//var preGenesisHash = "0000000000000000000000000000000000000000000000000000000000000000";
+//var time = Math.round((new Date()).getTime() / 1000);
+//var testBlock = new Block(preGenesisHash, transactions, time);
 
-var preGenesisHash = "0000000000000000000000000000000000000000000000000000000000000000";
-var time = Math.round((new Date()).getTime() / 1000);
-var testBlock = new Block(preGenesisHash, transactions, time);
-
-console.log(JSON.stringify(testBlock));
+//console.log(JSON.stringify(testBlock));
